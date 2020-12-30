c1a9593468c27d048ebaa17eb98d986e080bbd7a
my <3289371131@qq.com>
2020-12-30 22:03:02 +0800

    新的提交

--- src/components/a.vue
+++ src/components/a.vue
@@ -1,10 +1,147 @@
 <template>
-  <div class="hello">
-      <div id="a">
-          <span v-bind:title="message">
-              悬停
-          </span>
+<div class="box">
+ <div class="header">
+   <a class="header-left" href="#">深圳
+   <i class="iconfont iconsousuo"></i>
+   </a>
+   <div class="header-content">
+     <input type="text" placeholder="输入商家/品类/商圈" class="header-content-in">
+     <i class="iconfont iconsousuo" id="haha"></i>
+   </div>
+   <a href="#" class="right-a">
+     <i class="iconfont iconren" id="new-icon"></i>
+     <p class="right-p">我的</p>
+   </a>
+   
+ </div>
+ <div class="nav-a">
+   <ul class="nav-aa">
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+     <li><a href=""><span><i class="iconfont iconcanting" id="abs"></i><p>选择</p></span></a></li>
+   </ul>
+   <div class="anniu">
+     <div class="anniuc">
+       <div class="v"></div>
+       <div class="v"></div>
+     </div>
+   </div>
+ </div>
+<div class="a">
+  <div class="ji">
+  <div class="b">
+    <span class="b-a">
+      <p>我们一起</p>
+      <p>我们一起</p>
+    
+    <img src="../assets/person.png" alt="">
+    </span>
+  </div>
+  <div class="c">
+    <div class="cj">
+      <div class="qwe">
+        <span>
+          <p class="qwe-q">超低超值</p>
+          <p>十元与诗</p>
+        </span>
+        <img src="../assets/a.jpg" alt="">
       </div>
+      <div class="wqer">
+        <div class="we">
+          <span>
+            <p>我们一起前往</p>
+            <p>我们一起前往</p>
+          </span>
+          <img src="../assets/b.png" alt="">
+        </div>
+        <div class="we-right">
+          <span>
+            <p>我们一起前往</p>
+            <p>我们一起前往</p>
+          </span>
+          <img src="../assets/logo.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+
+<div class="thinke">
+  <div class="pp">
+    <p>猜你喜欢</p>
+  </div>
+  <ul class="ulul">
+    <li class="wangle">
+      <a href="#" class="bn">
+        <div class="aaa"><img src="../assets/jp.jpg" alt=""></div>
+        <div class="aaaa">
+          <h5 class="hh">真心鸡排</h5>
+          <p>你踩踩踩踩踩踩</p>
+          <p class="xa"><strong>9.5</strong><small>元</small><span>已售666</span></p>
+        </div>
+      </a>
+    </li>
+    <li class="wangle">
+      <a href="#" class="bn">
+        <div class="aaa"><img src="../assets/jp.jpg" alt=""></div>
+        <div class="aaaa">
+          <h5 class="hh">真心鸡排</h5>
+          <p>你踩踩踩踩踩踩</p>
+          <p class="xa"><strong>9.5</strong><small>元</small><span>已售666</span></p>
+        </div>
+      </a>
+    </li>
+    <li class="wangle">
+      <a href="#" class="bn">
+        <div class="aaa"><img src="../assets/jp.jpg" alt=""></div>
+        <div class="aaaa">
+          <h5 class="hh">真心鸡排</h5>
+          <p>你踩踩踩踩踩踩</p>
+          <p class="xa"><strong>9.5</strong><small>元</small><span>已售666</span></p>
+        </div>
+      </a>
+    </li>
+    <li class="wangle">
+      <a href="#" class="bn">
+        <div class="aaa"><img src="../assets/jp.jpg" alt=""></div>
+        <div class="aaaa">
+          <h5 class="hh">真心鸡排</h5>
+          <p>你踩踩踩踩踩踩</p>
+          <p class="xa"><strong>9.5</strong><small>元</small><span>已售666</span></p>
+        </div>
+      </a>
+    </li>
+    <li class="wangle">
+      <a href="#" class="bn">
+        <div class="aaa"><img src="../assets/jp.jpg" alt=""></div>
+        <div class="aaaa">
+          <h5 class="hh">真心鸡排</h5>
+          <p>你踩踩踩踩踩踩</p>
+          <p class="xa"><strong>9.5</strong><small>元</small><span>已售666</span></p>
+        </div>
+      </a>
+    </li>
+    <li class="wangle">
+      <a href="#" class="bn">
+        <div class="aaa"><img src="../assets/jp.jpg" alt=""></div>
+        <div class="aaaa">
+          <h5 class="hh">真心鸡排</h5>
+          <p>你踩踩踩踩踩踩</p>
+          <p class="xa"><strong>9.5</strong><small>元</small><span>已售666</span></p>
+        </div>
+      </a>
+    </li>
+    
+  </ul>
+</div>
   </div>
 </template>
 
@@ -25,14 +162,17 @@
 
 export default {
     
-  name: 'HelloWorld',
+  // name: 'HelloWorld',
   data () {
-   var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: '页面加载于 ' + new Date().toLocaleString()
-  }
-})
+    return{
+
+    }
+//    var app2 = new Vue({
+//   el: '#app-2',
+//   data: {
+//     message: '页面加载于 ' + new Date().toLocaleString()
+//   }
+// })
    
   }
   
@@ -55,6 +195,275 @@ li {
 a {
   color: #42b983;
 } */
+.b{
+  /* float: left; */
+  width: 200px;
+  height: 100%;
+  margin-top: 15px;
+    line-height: 80px;
+    flex: 1;
+}
+#abs{
+  position: static;
+  display: block;
+  line-height: 76.8px;
+  font-size: 40px;
+  margin-bottom: 12px;
+}
+.qwe-q{
+  display: inline-block;
+  margin-bottom: 32px;
+  margin-top: 20px;
+}
+.xa{
+  margin-top: 80px;
+}
+.hh{
+  margin: 0; 
+  margin-bottom: 20px;
+}
+.aaaa{
+  /* width: 110px;  */
+    /* display: grid; */
+    /* grid-gap: 9px; */
+    color: gray;
+    text-align: left;
+    margin-left: 31px;
+    width: 100%;
+}
+.nav-a{
+  height: 360px;
+
+  background: #fff;
+}
+.nav-aa span{
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+  background: yellow;
+  border-radius: 50%;
+  margin-top: 30px;
+}
+.wangle{
+  display: flex;
+    width: 100%;
+    /* border-top: 1px solid; */
+    border-bottom: 1px solid gainsboro;
+    height: 220px;
+}
+.xa span{
+  float: right;
+}
+.ulul{
+  padding: 20px;
+}
+.aaa img{
+  width: 190px;
+}
+.bn{
+  /* position: relative; */
+  display: flex;
+    width: 100%;
+    padding: 12px;
+}
+.anniu{
+
+  width: 60px;
+    margin: auto;
+    margin-top: 40px;
+}
+.v{
+  width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: whitesmoke;
+}
+.anniuc{
+  display: flex;
+    justify-content: space-between;
+}
+.pp{
+  width: 100%;
+  border-bottom: 1px solid gainsboro;
+}
+.pp p{
+  text-align: left;
+  padding-left: 60px;
+  font-size: 40px;
+  font-weight: 600;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+.thinke{
+  width: 100%;
+  margin-top: 30px;
+  background: #fff;
+}
+.c{
+  flex: 2;
+  /* height: px; */
+    margin-top: 4px;
+    /* display: grid; */
+}
+.cj{
+  height: 100%;
+  /* line-height: 60px; */
+  padding: 10px;
+  display: grid;
+}
+p{
+  margin: 0;
+}
+.we{
+  width: 49%;
+}
+.we-right{
+  width: 49%;
+}
+.we-right span{
+  display: inline-block;
+}
+.we-right img{
+  width: 100px;
+}
+.we span{
+  display: inline-block;
+}
+.wqer{
+  display: flex;
+}
+.we img{
+  width: 100px;
+}
+.qwe span{
+  display: inline-block;
+}
+.qwe img{
+width: 150px;
+}
+.b-a{
+  display: inline-block;
+}
+.a{
+  margin-top: 30px;
+  width: 100%;
+  height: 350px;
+  /* background: skyblue; */
+  background: #fff;
+}
+.van-swipe-item{
+  width: 100%;
+ padding: 0;
+ margin-top: 16px;
+}
+.van-swipe-item ul li i{
+  position: static;
+  display: grid;
+  font-size: 16px;
+  color: gray;
+  width: 100%;
+}
+.b-a img{
+  width: 150px;
+}
+.van-swipe-item{
+  /* padding: 25px; */
+  /* flex-shrink: unset; */
+}
+ul{
+  display: flex;
+  flex-wrap: wrap;
+  /* justify-content: space-between; */
+}
+li{
+  /* background: red; */
+  width: 20%;
+  height: 150px;
+}
+.ji{
+  display: flex;
+}
+.van-swipe{
+  height: 300px;
+}
+ .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 150px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
+.right-p{
+  margin-top: 30px;
+  /* margin-left: 50px; */
+  /* width: 160px; */
+  /* background: red; */
+  /* text-align: center; */
+  /* margin:50px 55px ; */
+  /* padding: 0; */
+  margin-top: 50px;
+  /* margin: 50px auto; */
+  /* width: 160px; */
+  width: 52px;
+  font-size: 30px;
+  font-variant-caps: all-petite-caps;
+  /* margin-left: 20px; */
+  text-align: center;
+}
+.right-a{
+  position: absolute;
+  right: 9px;
+  color:white;
+  top: 0;
+
+}
+/* body{
+  background: #f0efed;
+} */
+.van-swipe{
+  background: #fff;
+}
+#new-icon{
+  right: 0;
+  /* width: 160px; */
+  width: 100%;
+  font-size: 50px;
+  /* top: 30px; */
+}
+.header-content-in{
+  /* width: 400px; */
+  width: 100%;
+  text-align: center;
+  /* margin: auto; */
+  height: 50px;
+  font-size: 29px;
+  /* line-height: 50px; */
+  margin-top: 20px;
+  background-color: #05a494;
+  border: 1px solid #05a494;
+  border-radius: 6px;
+}
+#haha{
+  /* left: 0; */
+  left: 30px;
+  font-size: 40px;
+  top: 25px;
+}
+.header-content{
+  /* padding-left: 94px; */
+  /* padding-right: 94px; */
+  width: 500px;
+  /* margin: auto; */
+  position: absolute;
+  left: 160px;
+  height: 100px;
+}
+.iconfont{
+  position: absolute;
+  /* top: 5px; */
+  font-size: 30px;
+  
+}
 ul{
     list-style: none;
     display: flex;
@@ -63,5 +472,29 @@ ul{
 }
 input{
     width: 50px;
+}
+.header{
+  width: 100%;
+  height: 100px;
+  background: #06c1ae;
+  /* color: black; */
+}
+.header-left{
+  width: 150px;
+  /* height: 100%; */
+  height: 100px;
+  /* background: orangered; */
+  display: block;
+  position: absolute;
+  line-height: 100px;
+  font-size: 40px;
+  color: white;
+}
+.header-left-img{
+  width: 100%;
+  /* height: 100%; */
+}
+.header-left-img img{
+  width: 100%;
 }
 </style>
